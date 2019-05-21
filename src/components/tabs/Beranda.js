8ebd640ca40a0c84372059ad300b7bdd5ac7aeec
unknown <kukuh.baiquni@gmail.com>
2019-05-21 23:15:11 +0700

    implementation backhandler press (exit app confirmation)

--- src/components/tabs/Beranda.js
+++ src/components/tabs/Beranda.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, StyleSheet, ScrollView, Text, Image, AsyncStorage, ToastAndroid, Alert, StatusBar } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, ScrollView, Text, Image, AsyncStorage, ToastAndroid, Alert, StatusBar, BackHandler, Platform } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 import { connect } from 'react-redux';
 
@@ -9,10 +9,31 @@ import { setPlayerId } from '../../actions/Set_Player_Id';
 import { setInitialToken } from '../../actions/Set_Initial_Token';
 
 import NetInfo from "@react-native-community/netinfo";
+import RNExitApp from 'react-native-exit-app';
 
 class Beranda extends Component {
 
+    handleBackButton = () => {
+        Alert.alert(
+            'Konfirmasi',
+            'Apakah Anda yakin ingin keluar dari aplikasi?', [{
+                text: 'Batal',
+            }, {
+                text: 'OK',
+                onPress: () => RNExitApp.exitApp()
+            }, ], {
+                cancelable: false
+            }
+        )
+        return true;
+    };
+
+    _beforeBlur = () => {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+    };
+
     _beforeRender = async () => {
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
         NetInfo.fetch().then(state => {
             console.log("Connection type", state.type);
             console.log("Is connected?", state.isConnected);
@@ -23,13 +44,12 @@ class Beranda extends Component {
                     'Tidak ada Jaringan',
                     'Silahkan periksa kembali koneksi internet Anda',
                     [
-                        {text: 'OK'}
+                        {text: 'OK', onPress: () => RNExitApp.exitApp()}
                     ],
                     { cancelable: false }
                 );
             }
         });
-
     };
 
     _afterRender = async () => {
@@ -61,6 +81,7 @@ class Beranda extends Component {
             <View>
                 <NavigationEvents
                     onWillFocus={this._beforeRender}
+                    onWillBlur={this._beforeBlur}
                     />
                 <StatusBar
                     barStyle='light-content'
